57d11dc2d2bc978255cf3101a8f4f265778bd800
esparrago23 <142367155+esparrago23@users.noreply.github.com>
2023-12-01 07:43:10 -0600

    Add files via upload
    codigo landindpage

---
+++ dlog/jav.js
@@ -0,0 +1,94 @@
+//alert("que miras")
+//document.write("<h1>aguacate</h1>");
+//["asdlafm","fmsfkk"].forEach(alert)
+//const button = document.querySelector("button");
+ var numeroMes = prompt("indica el numero del mes");
+ var mes= parseInt(numeroMes);
+switch (mes) {
+    case 1:
+        console.log("Enero 31 dias");
+        break;
+    case 2:
+        alert("Febrero 28 dias ");
+        break;
+    case 3:
+        alert("Marzo 31 dias ");
+        break;
+    case 4:
+        alert("Abril 30 dias ");
+        break;
+    case 5:
+        alert("Mayo 31 dias ");
+        break;
+    case 6:
+        alert("Junio 30 dias ");
+        break;
+    case 7:
+        alert("Julio 31 dias ");
+        break;
+    case 8:
+        alert("Agosto 31 dias ");
+        break;
+    case 9:
+        alert("septiembre 30 dias ");
+        break;
+    case 10:
+        alert("octubre 31 dias ");
+        break;
+    case 11:
+        alert("Noviembre 30 dias ");
+        break;
+    case 12:
+        alert("Diciembre 31 dias ");
+        break;
+    default :
+    alert("error ");
+    break;
+    
+  }
+button.onclick = function () {
+  mes = prompt("indica el numero del mes");
+  switch (mes) {
+    case valor1:
+        alert("enero 31 dias ");
+      break;
+    case valor2:
+        alert("Febrero 28 dias ");
+        break;
+    case valor3:
+        alert("Marzo 31 dias ");
+        break;
+    case valor4:
+        alert("Abril 30 dias ");
+        break;
+    case valor5:
+        alert("Mayo 31 dias ");
+        break;
+    case valor6:
+        alert("Junio 30 dias ");
+        break;
+    case valor7:
+        alert("Julio 31 dias ");
+        break;
+    case valor8:
+        alert("Agosto 31 dias ");
+        break;
+    case valor9:
+        alert("septiembre 30 dias ");
+        break;
+    case valor10:
+        alert("octubre 31 dias ");
+        break;
+    case valor11:
+        alert("Noviembre 30 dias ");
+        break;
+    case valor12:
+        alert("Diciembre 31 dias ");
+        break;
+    default:
+        alert("ERROR");
+      break;
+  }
+}
+
+
